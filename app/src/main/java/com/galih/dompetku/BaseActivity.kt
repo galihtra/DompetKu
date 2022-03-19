78a776922e4c9721c5008248bbb87bd8ed5e0ed9
galihtra <galihtririskyandiko@gmail.com>
2022-03-18 22:26:43 -0700

    Initial commit

---
+++ app/src/main/java/com/galih/dompetku/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.galih.dompetku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
